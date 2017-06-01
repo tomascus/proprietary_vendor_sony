c21fe3ef215e5fbd04b95de3f137a8aa5912afe3
Ondrej Hlavac <hlavaco@gmail.com>
2017-06-01 11:39:29 +0200

    honami: add libta from shinano
    to fix macaddrsetup

--- honami/Android.mk
+++ honami/Android.mk
@@ -18,6 +18,16 @@ ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libta
+LOCAL_MODULE_OWNER := sony
+LOCAL_SRC_FILES := proprietary/vendor/lib/libta.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := sony
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
